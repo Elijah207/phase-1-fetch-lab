698f1d20bf328b41a0b7145869c3e90340ea14fd
elijah207 <elijahvanfuller@gmail.com>
2022-11-04 15:38:36 -0700

    commit tree fb6c0fcbe348b05e1d61a2b66546fb18a5a667cb

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-function fetchBooks() {
+// function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
-}
+
+  function fetchBooks() {
+    return fetch("https://anapioficeandfire.com/api/books")
+    .then(resp => resp.json())
+    .then(json => renderBooks(json))
+  }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+
+  for (const book of books){
+    console.log (book)
+ 
+  }
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
@@ -14,4 +24,18 @@ function renderBooks(books) {
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
+  const numArray = [1, 3, 4, 5, 6, 7] 
+
+//console.log(numArray [0])
+//consolle.log(numArray[1])
+
+//for (let i=0; 1 < numArray.length; i++){
+//  console.log(numArray(i))
+// }
+
+for (const num of numArray){
+  console.log(num)
+}
+
 });
